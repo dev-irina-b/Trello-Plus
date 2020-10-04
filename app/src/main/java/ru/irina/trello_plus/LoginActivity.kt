cc53ee8a310a93978399373fa622d0bfac15313f
irina <badertdinova289@gmail.com>
2020-10-04 20:40:40 +0700

    Add LoginActivity's layout
    - Change AppTheme's parent to MaterialComponents
    - Add Material library dependency

---
+++ app/src/main/java/ru/irina/trello_plus/LoginActivity.kt
@@ -0,0 +1,11 @@
+package ru.irina.trello_plus
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
