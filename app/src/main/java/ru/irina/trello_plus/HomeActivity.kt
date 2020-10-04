609ee8213c5bd78f1ca5a6ba42bdcd30d7874f8b
irina <badertdinova289@gmail.com>
2020-10-04 21:46:57 +0700

    Refactor MainActivity in HomeActivity

--- app/src/main/java/ru/irina/trello_plus/HomeActivity.kt
+++ app/src/main/java/ru/irina/trello_plus/HomeActivity.kt
@@ -3,10 +3,10 @@ package ru.irina.trello_plus
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
