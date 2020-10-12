985aacab5c2a21d62ff3324f72d048f1778a4d0f
irina <badertdinova289@gmail.com>
2020-10-12 21:40:52 +0700

    Add AppCompatActivity.toast extension method

--- app/src/main/java/ru/irina/trello_plus/Extensions.kt
+++ app/src/main/java/ru/irina/trello_plus/Extensions.kt
@@ -3,6 +3,9 @@ package ru.irina.trello_plus
 import android.content.Context
 import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
+import android.widget.Toast
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputLayout
 
 fun Context.getSP(): SharedPreferences = getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE)
@@ -10,3 +13,7 @@ fun Context.getSP(): SharedPreferences = getSharedPreferences(SHARED_PREFERENCES
 fun Context.getSPE(): SharedPreferences.Editor = getSP().edit()
 
 val TextInputLayout.text; get() = editText!!.text.toString()
+
+fun AppCompatActivity.toast(@StringRes stringRes: Int) {
+    Toast.makeText(this, stringRes, Toast.LENGTH_LONG).show()
+}
