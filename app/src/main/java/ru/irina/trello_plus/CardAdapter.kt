9051facda6305b5c486c8f7ee17d8bf2983ea41d
irina <badertdinova289@gmail.com>
2020-12-01 19:32:32 +0700

    If all checkItems are completed, checklist color is green

--- app/src/main/java/ru/irina/trello_plus/CardAdapter.kt
+++ app/src/main/java/ru/irina/trello_plus/CardAdapter.kt
@@ -1,10 +1,13 @@
 package ru.irina.trello_plus
 
+import android.content.res.ColorStateList
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_card.view.*
 
@@ -18,42 +21,53 @@ class CardAdapter(private val items: List<Card>, private val callback: DataCallb
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val currentCard = items[position]
+        val context = holder.itemView.context
         holder.name.text = currentCard.name
         holder.itemView.setOnClickListener {
             callback(currentCard)
         }
 
-        if(currentCard.desc.isBlank())
-            holder.descriptionIcon.visibility = View.GONE
-        else holder.descriptionIcon.visibility =View.VISIBLE
+        holder.descriptionIcon.visibility = if(currentCard.desc.isBlank()) View.GONE  else View.VISIBLE
 
-        if(currentCard.badges.comments != 0) {
-            holder.commentIcon.visibility = View.VISIBLE
-            holder.commentCount.visibility = View.VISIBLE
+        if(currentCard.badges.comments == 0) {
+            holder.commentLayout.visibility = View.GONE
+        } else {
+            holder.commentLayout.visibility = View.VISIBLE
             holder.commentCount.text = currentCard.badges.comments.toString()
         }
-        else holder.commentIcon.visibility =View.GONE
-
-        if(currentCard.badges.checkItems != 0) {
-            holder.checklistIcon.visibility = View.VISIBLE
-            holder.checkItemsChecked.visibility = View.VISIBLE
-            holder.slash.visibility = View.VISIBLE
-            holder.checkItems.visibility = View.VISIBLE
+        if(currentCard.badges.checkItems == 0) {
+            holder.checklistLayout.visibility = View.GONE
+        } else {
+            holder.checklistLayout.visibility = View.VISIBLE
             holder.checkItemsChecked.text = currentCard.badges.checkItemsChecked.toString()
             holder.checkItems.text = currentCard.badges.checkItems.toString()
         }
-        else holder.checklistIcon.visibility =View.GONE
+        if(currentCard.badges.checkItemsChecked == currentCard.badges.checkItems) {
+            val color = ContextCompat.getColor(context, R.color.green)
+            val colorStateList = ColorStateList.valueOf(color)
+            holder.checklistIcon.imageTintList = colorStateList
+            holder.checkItems.setTextColor(color)
+            holder.checkItemsChecked.setTextColor(color)
+            holder.slash.setTextColor(color)
+        } else {
+            val color = ContextCompat.getColor(context, R.color.black)
+            val colorStateList = ColorStateList.valueOf(color)
+            holder.checklistIcon.imageTintList = colorStateList
+            holder.checkItems.setTextColor(color)
+            holder.checkItemsChecked.setTextColor(color)
+            holder.slash.setTextColor(color)
+        }
     }
 
     class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
         val name: TextView = v.name
         val descriptionIcon: ImageView = v.descIcon
-        val commentIcon: ImageView = v.commentIcon
         val commentCount: TextView = v.commentCount
-        val checklistIcon: ImageView = v.checklistIcon
         val checkItemsChecked: TextView = v.checkItemsChecked
-        val slash: TextView = v.slash
         val checkItems: TextView = v.checkItems
-
+        val slash: TextView = v.slash
+        val commentLayout: LinearLayout = v.commentLayout
+        val checklistLayout: LinearLayout = v.checklistLayout
+        val checklistIcon: ImageView = v.checklistIcon
     }
 }
