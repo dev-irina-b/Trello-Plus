94f0d4a1a8d4e5fd5a8d401e2864c4c0e96b59d3
irina <badertdinova289@gmail.com>
2021-01-05 22:36:00 +0800

    Add ability to collapse the checklist

--- app/src/main/java/ru/irina/trello_plus/CheckList.kt
+++ app/src/main/java/ru/irina/trello_plus/CheckList.kt
@@ -7,7 +7,7 @@ data class CheckList (
     val name: String,
     val checkItems: List<CheckListItem>
 ) {
-
+    var collapsed: Boolean = false
     @JsonIgnoreProperties(ignoreUnknown = true)
     data class CheckListItem (
         val name: String,
--- app/src/main/java/ru/irina/trello_plus/CheckListAdapter.kt
+++ app/src/main/java/ru/irina/trello_plus/CheckListAdapter.kt
@@ -3,6 +3,7 @@ package ru.irina.trello_plus
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_check_list.view.*
@@ -22,12 +23,22 @@ class CheckListAdapter(private val items: List<CheckList>) : RecyclerView.Adapte
         val currentCheckList = items[position]
         holder.checkListTitle.text = currentCheckList.name
         holder.checkListItemRecycler.adapter = CheckListItemAdapter(currentCheckList.checkItems)
+        holder.arrowIcon.setOnClickListener {
+            currentCheckList.collapsed = !currentCheckList.collapsed
+            holder.setCollapsedState(currentCheckList.collapsed)
+        }
     }
 
     override fun getItemCount() = items.size
 
     class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
-        val checkListTitle : TextView = v.checkListTitle
-        val checkListItemRecycler : RecyclerView = v.checkListItemRecycler
+        val checkListTitle: TextView = v.checkListTitle
+        val checkListItemRecycler: RecyclerView = v.checkListItemRecycler
+        val arrowIcon: ImageView = v.arrowIcon
+
+        fun setCollapsedState(collapsed: Boolean) {
+            arrowIcon.setImageResource( if(collapsed) R.drawable.arrow_down_black_24 else R.drawable.arrow_up_black_24)
+            checkListItemRecycler.visibility = if(collapsed) View.GONE else View.VISIBLE
+        }
     }
 }
